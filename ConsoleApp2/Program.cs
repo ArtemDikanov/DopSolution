a0aac89a08ef1ae75cfb3218958dd8b9a2d52e49
Tyuiu.DikanovAA@outlook.com <Tyuiu.DikanovAA@outlook.com>
2024-11-28 19:20:55 +0500

    Sprint3Variant5

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using Library2;
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = ds.Metod2(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.Metod2(startValue, stopValue);
+
+            Console.WriteLine("+------------+-------------+");
+            Console.WriteLine("|     X      |      f(x)   |");
+            Console.WriteLine("+------------+-------------+");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("| {0,5:d}      |   {1, 6:f2}    |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+------------+-------------+");
+            Console.ReadKey();
+        }
+    }
+}
